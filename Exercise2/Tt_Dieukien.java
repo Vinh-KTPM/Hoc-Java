1973170bfef5e2935f9165863b0691a9a8347e0e
Vinh KTPM <thanhvinh.ktpm@gmail.com>
2023-04-19 14:43:21 +0700

    Toan tu dieu kien

---
+++ Exercise2/Tt_Dieukien.java
@@ -0,0 +1,9 @@
+package Exercise2;
+
+public class Tt_Dieukien {
+    public static void main(String[] args) {
+        char ch = '0';
+        String s = (ch >= '0' && ch <= '9')? "Ký tự số" : "Ký tự chữ";
+        System.out.println(s);
+    }
+}
