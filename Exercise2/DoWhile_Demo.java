1fd15a20d5412ea92b5d691695244ae74da9aad1
Vinh KTPM <thanhvinh.ktpm@gmail.com>
2023-04-19 14:39:43 +0700

    DoWhile

---
+++ Exercise2/DoWhile_Demo.java
@@ -0,0 +1,11 @@
+package Exercise2;
+
+public class DoWhile_Demo {
+    public static void main(String[] args) {
+        int i = 1;
+        do{
+            System.out.println("Giá trị của i là : " +i);
+            i++;
+        }while (i <= 10);
+    }
+}
