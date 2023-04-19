f0a86ba85e074f9abfea9c90de75cb4e147fdf08
Vinh KTPM <thanhvinh.ktpm@gmail.com>
2023-04-19 14:40:24 +0700

    For

---
+++ Exercise2/For_Demo.java
@@ -0,0 +1,11 @@
+package Exercise2;
+
+public class For_Demo {
+    public static void main(String[] args) {
+        int sum = 0;
+        for (int i = 1; i <=10; i++){
+            sum += i;
+        }
+        System.out.println("Tổng các số từ 1 đến 10 là: " + sum);
+    }
+}
