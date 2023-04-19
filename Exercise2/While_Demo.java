add33698acb10b6a7ada890d3b16a587c8548128
Vinh KTPM <thanhvinh.ktpm@gmail.com>
2023-04-19 14:45:09 +0700

    While

---
+++ Exercise2/While_Demo.java
@@ -0,0 +1,11 @@
+package Exercise2;
+
+public class While_Demo {
+    public static void main(String[] args) {
+        int i = 0;
+        while(i <= 10){
+            System.out.println("Giá trị của i là: " +i );
+            i++;
+        }
+    }
+}
